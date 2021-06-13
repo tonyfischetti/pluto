e7c1aedc7539090e312362fbb821d1ff39847e24
Tony Fischetti <tony.fischetti@gmail.com>
2021-06-13 14:22:35 -0400

    step 1

--- tests/def-test-doc.lisp
+++ tests/def-test-doc.lisp
@@ -12,16 +12,16 @@
 
 ;; -----------------------------------
 ;; classes
-(defclass test-doc-element ()
+(defclass test/doc-element ()
   ((section :initarg :section :initform /test-section/)
    (doc :initarg :doc :initform "")))
 
-(defclass test-doc-title (test-doc-element)
+(defclass test/doc-title (test/doc-element)
   ((tagline :initarg :tagline)))
 
-(defclass test-doc-section (test-doc-element) ())
+(defclass test/doc-section (test/doc-element) ())
 
-(defclass test-doc-test (test-doc-element)
+(defclass test/doc-test (test/doc-element)
   ((test-number :initform (incf /test-counter/))
    (the-function :initarg :the-function)
    (test-closure :initarg :test-closure)
@@ -30,18 +30,18 @@
    (pass-p :initform nil)
    output))
 
-(defclass test-doc-test-returns (test-doc-test) ())
+(defclass test/doc-test-returns (test/doc-test) ())
 
-(defclass test-doc-test-stdout (test-doc-test) ())
+(defclass test/doc-test-stdout  (test/doc-test) ())
 
-(defclass test-doc-test-stderr (test-doc-test) ())
+(defclass test/doc-test-stderr  (test/doc-test) ())
 ;; -----------------------------------
 
 
 (defun start-tests (&key title tagline doc)
   (setq /all-test-docs/ nil)
   (setq /test-counter/ 0)
-  (push (make-instance 'test-doc-title
+  (push (make-instance 'test/doc-title
                        :section title
                        :tagline tagline
                        :doc doc) /all-test-docs/))
@@ -52,20 +52,20 @@
 
 ;; -----------------------------------
 ;; "constructors"
-(defun def-test-title (title)
-  (push (make-instance 'test-doc-title :doc title) /all-test-docs/))
+(defun def-test/doc-title (title)
+  (push (make-instance 'test/doc-title :doc title) /all-test-docs/))
 
-(defun def-test-section (section-name)
+(defun def-test/doc-section (section-name)
   (setq /test-section/ section-name)
-  (push (make-instance 'test-doc-section) /all-test-docs/))
+  (push (make-instance 'test/doc-section) /all-test-docs/))
 
-(defmacro def-test-doc (&body body)
+(defmacro def-test/doc-test (&body body)
   (destructuring-bind (the-function doc test-type test &rest code) body
     `(push (make-instance
              (ecase ,test-type
-               ('returns  'test-doc-test-returns)
-               ('stdout   'test-doc-test-stdout)
-               ('stderr   'test-doc-test-stderr))
+               ('returns  'test/doc-test-returns)
+               ('stdout   'test/doc-test-stdout)
+               ('stderr   'test/doc-test-stderr))
              :the-function ,the-function
              :doc ,doc
              :test-closure (lambda () ,test)
@@ -78,15 +78,15 @@
 
 ;; -----------------------------------
 ;; testing
-(defgeneric run-test (test-doc-element))
+(defgeneric run-test (test/doc-element))
 
-(defmethod run-test ((test test-doc-title))
+(defmethod run-test ((test test/doc-title))
   (ft (cyan "beginning tests for ~A~%" { test 'section })))
 
-(defmethod run-test ((test test-doc-section))
+(defmethod run-test ((test test/doc-section))
   (ft (yellow "testing section: ~A~%" { test 'section })))
 
-(defmethod run-test ((test test-doc-test))
+(defmethod run-test ((test test/doc-test))
   (multiple-value-bind (test-return-value! test-stdout! test-stderr!)
     (capture-all-outputs { test 'code-closure })
     (let ((test-result (funcall { test 'test-closure })))
@@ -98,9 +98,9 @@
                 `(,test-return-value! ,test-stdout! ,test-stderr!)))
         (ft (red "failed: ~A~%" { test 'the-function }))))))
 
-(defgeneric passed-p  (test-doc-element))
-(defmethod passed-p   ((test test-doc-element)) t)
-(defmethod passed-p   ((test test-doc-test)) { test 'pass-p })
+(defgeneric passed-p  (test/doc-element))
+(defmethod passed-p   ((test test/doc-element)) t)
+(defmethod passed-p   ((test test/doc-test)) { test 'pass-p })
 
 (defun run-tests ()
   (mapcar #'run-test /all-test-docs/)
@@ -110,30 +110,30 @@
 
 ;; -----------------------------------
 ;; markdown
-(defgeneric to-markdown (test-doc-element &optional stream))
+(defgeneric to-markdown (test/doc-element &optional stream))
 
-(defmethod to-markdown ((test test-doc-title) &optional (stream t))
+(defmethod to-markdown ((test test/doc-title) &optional (stream t))
   (format stream "# ~A documentation~%_~A_~%~%~A~%"
           { test 'section }
           { test 'tagline }
           { test 'doc }))
 
-(defmethod to-markdown ((test test-doc-section) &optional (stream t))
+(defmethod to-markdown ((test test/doc-section) &optional (stream t))
   (format stream "~%-----~%~%# ~A~%~%" { test 'section }))
 
-(defmethod to-markdown ((test test-doc-test) &optional (stream t))
+(defmethod to-markdown ((test test/doc-test) &optional (stream t))
   (format stream "~%### ~A~%~%" { test 'the-function })
   (format stream "```~%~S~%```~%~%" (car { test 'raw-code })))
 
-(defmethod to-markdown :after ((test test-doc-test-returns)
+(defmethod to-markdown :after ((test test/doc-test-returns)
                                &optional (stream t))
   (format stream "Returns:~%```~%~A~%```~%~%" (car { test 'output })))
 
-(defmethod to-markdown :after ((test test-doc-test-stdout)
+(defmethod to-markdown :after ((test test/doc-test-stdout)
                                &optional (stream t))
   (format stream "Outputs:~%```~%~A~%```~%~%" (cadr { test 'output })))
 
-(defmethod to-markdown :after ((test test-doc-test-stderr)
+(defmethod to-markdown :after ((test test/doc-test-stderr)
                                &optional (stream t))
   (format stream "Standard error:~%```~%~A~%```~%~%" (caddr { test 'output })))
 
--- tests/test-pluto.lisp
+++ tests/test-pluto.lisp
@@ -12,9 +12,9 @@
 
 ; --------------------------------------------------------------- ;
 
-(def-test-section "some essential utilities/macros")
+(def-test/doc-section "some essential utilities/macros")
 
-(def-test-doc '-<>
+(def-test/doc-test '-<>
   "test doc"
   'returns
   (= test-return-value! 2)
@@ -24,16 +24,16 @@
 
 ; --------------------------------------------------------------- ;
 
-(def-test-section "for-each and friends")
+(def-test/doc-section "for-each and friends")
 
-(def-test-doc 'for-each
+(def-test/doc-test 'for-each
   "test doc"
   'stdout
   (string= test-stdout! (fn "1 -> A~%2 -> B~%3 -> C~%"))
   (for-each/list '(a b c)
     (format t "~A -> ~A~%" index! value!)))
 
-(def-test-doc 'for-each
+(def-test/doc-test 'for-each
   "test doc"
   'stdout
   (string= test-stdout! (fn "A~%B~%"))
@@ -41,7 +41,7 @@
     (if (> index! 2) (break!))
     (format t "~A~%" value!)))
 
-(def-test-doc 'for-each
+(def-test/doc-test 'for-each
   "test doc"
   'stdout
   (string= test-stdout! (fn "A~%B~%D~%E~%"))
@@ -49,7 +49,7 @@
     (if (= index! 3) (continue!))
     (format t "~A~%" value!)))
 
-(def-test-doc 'for-each
+(def-test/doc-test 'for-each
   "for-each/line"
   'stdout
   (string= test-stdout! (fn "1 -> we gotta celebrate diversity~%2 -> in the university~%"))
@@ -58,7 +58,7 @@
 
 ; TODO: warning about undefined variable: PLUTO:KEY!
 ; that doesn't happen when using for-each/hash
-(def-test-doc 'for-each
+(def-test/doc-test 'for-each
   "for-each/hash"
   'stdout
   (or (string= test-stdout! (fn "GREEN -> veridian~%RED -> cadmium~%"))
@@ -71,15 +71,15 @@
 
 ; --------------------------------------------------------------- ;
 
-(def-test-section "other abbreviations and shortcuts")
+(def-test/doc-section "other abbreviations and shortcuts")
 
-(def-test-doc 'get-size
+(def-test/doc-test 'get-size
   "test doc"
   'returns
   (string= test-return-value! "17k")
   (get-size "interior-of-a-heart.txt"))
 
-(def-test-doc 'get-size
+(def-test/doc-test 'get-size
   "test doc"
   'returns
   (= test-return-value! 14433)
